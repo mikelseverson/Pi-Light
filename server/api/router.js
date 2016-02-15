8613a2f871a83830b8d285771294e4b187978ee5
Mikel Severson <mkseve@gmail.com>
2016-02-15 10:23:00 -0600

    more lights

--- server/api/router.js
+++ server/api/router.js
@@ -22,8 +22,10 @@ var Led = function (pin) {
 	gpio.setup(this.pin, gpio.DIR_OUT);
 };
 
-var led7 = new Led(11);
-var led11 = new Led(7)
+var led7 = new Led(7);
+var led11 = new Led(11);
+var led18 = new Led(18);
+var led22 = new Led(22);
 
 
 router.get('/', function(req, res) {
@@ -56,6 +58,8 @@ router.get('/on/:led', function(req, res) {
 
 router.get('/on', function(req, res){
 	led7.turnOn();
+	led18.turnOn();
+	led22.turnOn();
 	led11.turnOn(function() {
 			res.json({
 				led7: led7.on,
@@ -86,6 +90,8 @@ router.get('/off/:led', function(req, res) {
 
 router.get('/off', function(req, res) {
 	led7.turnOff();
+	led18.turnOff();
+	led22.turnOff();
 	led11.turnOff(function() {
 			res.json({
 				led7: led7.on,
