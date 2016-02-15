f4c75b40f5ffbd6d512630ecab530f1981bca411
Mikel Severson <mkseve@gmail.com>
2016-02-15 07:23:39 -0600

    sanity check

--- write-to-pin.js
+++ write-to-pin.js
@@ -11,12 +11,14 @@ var Led = function (pin) {
 		gpio.write(this.pin, true, (err) => {
 			if(err) throw err;
 			this.on = true;
+			return callback()
 		})
 	}
 	this.turnOff = function(callback) {
 		gpio.write(this.pin, false, (err) => {
 			if(err) throw err;
 			this.on = false;
+			return callback()
 		})
 	}
 	gpio.setup(this.pin, gpio.DIR_OUT);
@@ -24,6 +26,7 @@ var Led = function (pin) {
 
 var led7 = new Led(11);
 var led11 = new Led(7)
+
 
 app.get('/', function(req, res) {
 	res.json({
@@ -35,50 +38,62 @@ app.get('/', function(req, res) {
 app.get('/on/:led', function(req, res) {
 	led = req.params.led
 	if(led == 7) {
-		led7.turnOn();
+		led7.turnOn(function() {
+				res.json({
+					led7: led7.on,
+					led11: led11.on
+				});
+			})
 	}
 	else if(led == 11) {
-		led11.turnOn();
+		led11.turnOn(function() {
+				res.json({
+					led7: led7.on,
+					led11: led11.on
+				});
+			})
 	}
-	setTimeout(100);
-	res.json({
-		led7: led7.on,
-		led11: led11.on
-	})
 });
+
+
 app.get('/on', function(req, res){
 	led7.turnOn();
-	led11.turnOn();
-	setTimeout(100);
-	res.json({
-		led7: led7.on,
-		led11: led11.on
-	});
+	led11.turnOn(function() {
+			res.json({
+				led7: led7.on,
+				led11: led11.on
+			});
+		});
 });
 
 app.get('/off/:led', function(req, res) {
 	led = req.params.led
 	if(led == 7) {
-		led7.turnOff();
+		led7.turnOff(function() {
+				res.json({
+					led7: led7.on,
+					led11: led11.on
+				});
+			})
 	}
 	else if(led == 11) {
-		led11.turnOff();
+		led11.turnOff(function() {
+				res.json({
+					led7: led7.on,
+					led11: led11.on
+				});
+			})
 	}
-	setTimeout(100);
-	res.json({
-		led7: led7.on,
-		led11: led11.on
-	});
 });
 
 app.get('/off', function(req, res) {
 	led7.turnOff();
-	led11.turnOff();
-	setTimeout(100);
-	res.json({
-		led7: led7.on,
-		led11: led11.on
-	});
+	led11.turnOff(function() {
+			res.json({
+				led7: led7.on,
+				led11: led11.on
+			});
+		})
 });
 
 app.listen(3000, function() {
