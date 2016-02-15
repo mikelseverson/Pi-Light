45d2a9d7b9b3c60308bf479f4a35e8793a700615
Mikel Severson <mkseve@gmail.com>
2016-02-15 08:39:42 -0600

    fixed merge errors

--- server/api/router.js
+++ server/api/router.js
@@ -1,23 +1,103 @@
 var router = require('express').Router();
+var gpio = require('rpi-gpio');
 
-// This may be a great place to add middleware for API requests
-// I would like any API request to require auth so I am hoping I can
-// throw an error when this point is accessed without auth
 
-//this will happen when there are requests to /api/*
-router.use((req, res, next) => {
-	console.log('hit');
-	next();
+var Led = function (pin) {
+	this.on = false;
+	this.pin = pin;
+	this.turnOn = function(callback) {
+		gpio.write(this.pin, true, (err) => {
+			if(err) throw err;
+			this.on = true;
+			if(callback) return callback()
+		})
+	}
+	this.turnOff = function(callback) {
+		gpio.write(this.pin, false, (err) => {
+			if(err) throw err;
+			this.on = false;
+			if(callback) return callback()
+		})
+	}
+	gpio.setup(this.pin, gpio.DIR_OUT);
+};
+
+var led7 = new Led(11);
+var led11 = new Led(7)
+
+
+app.get('/', function(req, res) {
+	res.json({
+		led7: led7.on,
+		led11: led11.on
+	});
 });
 
-router.use('/test', require('./test/testRoutes'));
+app.get('/on/:led', function(req, res) {
+	led = req.params.led
+	if(led == 7) {
+		led7.turnOn(function() {
+				res.json({
+					led7: led7.on,
+					led11: led11.on
+				});
+			})
+	}
+	else if(led == 11) {
+		led11.turnOn(function() {
+				res.json({
+					led7: led7.on,
+					led11: led11.on
+				});
+			})
+	}
+});
 
-//api catchall so it doesn't move on to frontend requests 
+
+app.get('/on', function(req, res){
+	led7.turnOn();
+	led11.turnOn(function() {
+			res.json({
+				led7: led7.on,
+				led11: led11.on
+			});
+		});
+});
+
+app.get('/off/:led', function(req, res) {
+	led = req.params.led
+	if(led == 7) {
+		led7.turnOff(function() {
+				res.json({
+					led7: led7.on,
+					led11: led11.on
+				});
+			})
+	}
+	else if(led == 11) {
+		led11.turnOff(function() {
+				res.json({
+					led7: led7.on,
+					led11: led11.on
+				});
+			})
+	}
+});
+
+app.get('/off', function(req, res) {
+	led7.turnOff();
+	led11.turnOff(function() {
+			res.json({
+				led7: led7.on,
+				led11: led11.on
+			});
+		})
+});
+
+//api catchall so it doesn't move on to frontend requests
 //the public router will look for a file in dist/api/* without this
 router.get("/*", function(req, res) {
     res.send('Not found');
 });
 
 module.exports = router;
-
-
