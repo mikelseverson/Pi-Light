a1b13ffd4a369c755b409673e36de45b59450ff6
Mikel Severson <mkseve@gmail.com>
2016-02-15 00:34:00 -0600

    experiment with independent led onoff

--- write-to-pin.js
+++ write-to-pin.js
@@ -26,13 +26,23 @@ var led11 = new Led(7)
 
 app.get('/', function(req, res) {
 	res.send("led7: " + led7.on + " led11: " + led11.on);
-
 });
 
-app.get('/on', function(req, res) {
-	led7.turnOn();
-	led11.turnOn();
-	res.send('Turning them on');
+app.get('/on/:led', function(req, res) {
+	led = req.params.led
+	if(led == 7) {
+		led7.turnOn();
+		res.send('turned on 7');
+	}
+	else if(led == 11) {
+		led11.turnOn();
+		res.send('turned on 11');
+	}
+	else {
+		led7.turnOn();
+		led11.turnOn();
+		res.send('turning all leds on');
+	}
 });
 
 app.get('/off', function(req, res) {
