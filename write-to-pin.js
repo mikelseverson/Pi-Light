b17f649eed02266912bc4cf615562e546c589954
Mikel Severson <mkseve@gmail.com>
2016-02-15 00:26:23 -0600

    testing object oriented approach

--- write-to-pin.js
+++ write-to-pin.js
@@ -3,38 +3,38 @@ var express = require('express');
 
 app = express();
 
-var status = 'off';
-gpio.setup(7, gpio.DIR_OUT);
-gpio.setup(11, gpio.DIR_OUT);
+var Led = function (pin) {
+	this.on = false;
+	this.pin = pin
+	this.turnOn = function() {
+		gpio.write(led, true, (err) => {
+			if(err) throw err;
+			this.on = true;
+		})
+	}
+	gpio.setup(this.pin, gpio.DIR_OUT);
+};
+
+var led7 = new Led(11);
+var led11 = new Led(7)
 
 app.get('/', function(req, res) {
-	res.send("Hey, the light is " + status);
-	
+	res.send("led7: " + led7.on + " led11: " + led11.on);
+
 });
 
 app.get('/on', function(req, res) {
-	gpio.write(11, true);
-    
-gpio.write(7, true, function(err) {
-        if(err) throw err;
-	console.log('light turned on');
-	status = "on"
-	res.send("You turned it on");
-	});
+	led7.turnOn();
+	led11.turnOn();
+	res.send('Turning them on');
 });
 
 app.get('/off', function(req, res) {
-	gpio.write(11, false);
-
-	gpio.write(7, false, function(err) {
-		if(err) throw err;
-		console.log('light turned off');
-		res.send("you turned it off");
-		status = "off";	
-	});
+	led7.turnOff();
+	led11.turnOff();
+	res.send('Turning them off');
 });
 
 app.listen(3000, function() {
-	console.log('listening');
-
+	console.log('listening on port 3000');
 });
